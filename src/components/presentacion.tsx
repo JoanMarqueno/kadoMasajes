ab0cfe2ac413f2f02e76548ea8e1b4b3249bbb02
joanm <joan.marquenyo@gmail.com>
2023-12-01 22:57:50 +0100

    Update video source in Presentacion component

--- src/components/presentacion.tsx
+++ src/components/presentacion.tsx
@@ -6,25 +6,23 @@ export default function Presentacion() {
         alt="Descripción de la imagen"
         className="absolute inset-0 w-full h-[28rem] sm:h-[32rem] object-cover  z-0 block"
       /> */}
-      <video className="absolute inset-0 w-full h-[28rem] sm:h-[32rem] object-cover  z-0 block" width="500" autoPlay loop muted>
-            <source src="https://carontestudio.com/img/f4.mp4" type="video/mp4"/>
-          
-        </video>
+      <video className="absolute inset-0 w-full h-[28rem] sm:h-[32rem] object-cover  z-0 block" width="500" loop autoPlay playsInline muted>
+        <source src="https://carontestudio.com/img/f4.mp4" type="video/mp4" />
+      </video>
       <div className="absolute inset-0 w-full h-[28rem] sm:h-[32rem] bg-gradient-to-tr from-color-letra-nav to-color-letra-card opacity-60"></div>
 
       <div className="mx-auto max-w-2xl lg:max-w-4xl relative flex justify-center items-center h-full">
         <figure className="sm:mt-36 mt-20 text-center">
-          <blockquote className="text-xl leading-8 mx-2 sm:mx-0  text-color-letra-blanca sm:text-2xl sm:leading-9 overflow-hidden" style={{ fontFamily: 'Montserrat' }}>
-            <p>
-              “Kadō Masajes es un espacio único en pleno centro de Tarragona consagrado al bienestar y a la relajación”
-            </p>
-            <br/>
-            <p>
-              Estás a un masaje de sentirte mejor…
-            </p>  
+          <blockquote
+            className="text-xl leading-8 mx-2 sm:mx-0  text-color-letra-blanca sm:text-2xl sm:leading-9 overflow-hidden"
+            style={{ fontFamily: 'Montserrat' }}
+          >
+            <p>“Kadō Masajes es un espacio único en pleno centro de Tarragona consagrado al bienestar y a la relajación”</p>
+            <br />
+            <p>Estás a un masaje de sentirte mejor…</p>
           </blockquote>
         </figure>
-      </div>  
+      </div>
     </section>
   );
 }
