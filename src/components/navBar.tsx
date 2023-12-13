7fbfe68a522c677bc58506a4058c045918518bf0
joanm <joan.marquenyo@gmail.com>
2023-12-13 19:11:10 +0100

    Update navBar component

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import Hamburger from "hamburger-react";
 import { Transition } from "@headlessui/react";
 import { NavLinkDesktop } from "./navLink";
@@ -142,7 +142,7 @@ export default function Navbar() {
               >
                 <div className="flex flex-col lg:flex-row lg:mx-6">
                   {navigation.map((item) => (
-                    <ScrollLink
+                    <NavLink
                       key={item.name}
                       to={item.href}
                       className={classNames(
@@ -151,10 +151,13 @@ export default function Navbar() {
                           : "",
                         "px-3 py-5 relative"
                       )}
-                      onClick={handleMenuClick}
+                      onClick={() => {
+                        handleMenuClick();
+                        window.scrollTo(0, 0);
+                      }}
                     >
                       {item.name}
-                    </ScrollLink>
+                    </NavLink>
                   ))}
                   <div className="flex flex-col-reverse justify-between  py-4 border-t lg:flex-row"></div>
                   <address className="flex justify-around items-center">
