f4e1d51d5717b894dcfc07078667d9ed91fd78c2
joanm <joan.marquenyo@gmail.com>
2024-05-21 11:16:09 +0200

    remove piedras calientes

--- src/components/presentacion.tsx
+++ src/components/presentacion.tsx
@@ -8,16 +8,19 @@ interface Props {
 
 export default function Presentacion({ datos, urlFoto }: Props) {
   return (
-    <section className="w-full h-[28rem] sm:h-[32rem]">
+    <section className="relative w-full h-[28rem] sm:h-[32rem]">
+      {/* Espacio reservado para la imagen */}
+      <div className="absolute inset-0 w-full h-[28rem] sm:h-[32rem] bg-gray-200"></div>
+
+      {/* Imagen superpuesta */}
       <img
         src={urlFoto}
         alt="Imagen de la sala con una camilla y unas flores"
-        className="absolute inset-0 w-full h-[28rem] sm:h-[32rem] object-cover  z-0 block"
+        className="absolute inset-0 w-full h-[28rem] sm:h-[32rem] object-cover z-0"
       />
 
-      <div className="absolute inset-0 w-full h-[28rem] sm:h-[32rem] bg-gradient-to-tr from-color-letra-nav to-color-letra-card opacity-60"></div>
-
-      <div className="mx-auto max-w-2xl lg:max-w-4xl relative flex justify-center items-center h-full">
+      {/* Contenido */}
+      <div className="mx-auto max-w-2xl lg:max-w-4xl relative flex justify-center items-center h-full z-10">
         <figure className="sm:mt-36 mt-20 text-center">
           <blockquote className="text-xl leading-8 mx-2 sm:mx-0  text-color-letra-blanca sm:text-2xl sm:leading-9 overflow-hidden">
             <p>{datos.titulo}</p>
